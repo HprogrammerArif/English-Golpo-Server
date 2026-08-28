dbfec903eb8f285885a0d6e4de4ffaf46c1892cc
hProgrammerArif <work.mohammedarif@gmail.com>
2026-08-28 20:14:36 +0600

    build: generate distribution files and source maps for application module and controller

--- dist/src/app.module.js
+++ dist/src/app.module.js
@@ -11,6 +11,8 @@ const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
 const throttler_1 = require("@nestjs/throttler");
 const schedule_1 = require("@nestjs/schedule");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
 const prisma_module_1 = require("./prisma/prisma.module");
 const auth_module_1 = require("./modules/auth/auth.module");
 const user_module_1 = require("./modules/user/user.module");
@@ -30,6 +32,8 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
         imports: [
             config_1.ConfigModule.forRoot({
                 isGlobal: true,
